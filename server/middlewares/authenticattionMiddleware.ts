272eb62db820e7ec79d7719e8cb8a895ddeab126
leandrosantino <leandrosantino2013@gmail.com>
2023-06-16 21:08:42 -0300

    Add commit lint

--- server/middlewares/authenticattionMiddleware.ts
+++ server/middlewares/authenticattionMiddleware.ts
@@ -1,6 +1,6 @@
 import { initTRPC } from '@trpc/server'
 import { Context } from '../context';
-import { JwtProvider } from '../providers/implementations/JwtProvider'
+import { JwtProvider } from "../providers/implementations/JwtProvider"
 import { HttpError } from '../utils/HttpError';
 import { IJwtUserData } from '../providers/IJwtProvider';
 const t = initTRPC.context<Context>().create()
