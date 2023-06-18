e502b9fb9b499ccde3dec1846511d18735dc6b12
leandrosantino <leandrosantino2013@gmail.com>
2023-06-18 10:37:17 -0300

    test: add jest
    Adicionado o jest, feito as configurações iniciais e criado o test do caso de uso create user

---
+++ server/useCases/CreateUser/createUser.spec.ts
@@ -0,0 +1,36 @@
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+import { ISystemPermissionsRepository } from '../../repositories/ISystemPermissionsRepository'
+import { CreateUser } from './CreateUser'
+
+test('Use Case : Create User', async () => {
+
+  const userRepoMock = {
+    async findByName(userName) {
+      return userName == 'leandro' ? {} : null
+    },
+    async create(data) {
+      return data ? {} : ''
+    },
+  } as IUsersRepository
+  const systemPermissionsRepoMock = {
+    async findManyById(ids) {
+      return [
+        { id: ids[0], description: 'CREATE_USERS' },
+        { id: ids[1], description: 'PLANNING' }
+      ]
+    },
+  } as ISystemPermissionsRepository
+
+  const createUser = new CreateUser(
+    userRepoMock,
+    systemPermissionsRepoMock
+  )
+
+  expect(await createUser.execute({
+    email: 'leandrosantino',
+    name: 'leandro',
+    password: '12345',
+    permissions: [1, 2]
+  })).toStrictEqual({ message: 'User registered successfully' })
+
+})
