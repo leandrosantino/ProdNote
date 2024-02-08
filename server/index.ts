9994975edcec36799b682c7f2ac9ef6d5c22e852
leandrosantino <leandrosantino2013@gmail.com>
2024-02-08 09:27:48 -0300

    update

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,18 @@ import dotenv from 'dotenv'
 import { backupService } from './services/BackupService'
 dotenv.config()
 
+const port = process.env.PORT as string
+
+if (!port) {
+  throw Error('port is note defined')
+}
+
 const server = new HttpServer(
   backupService,
   {
     apiEndpoint: '/api',
     playgroundEndpoint: '/playground',
-    port: 3336,
+    port: Number(port),
     staticsDirectory: path.join(__dirname, './static')
   }
 )
